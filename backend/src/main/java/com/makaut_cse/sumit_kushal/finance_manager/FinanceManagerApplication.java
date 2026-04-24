6ec300b8d31f3344a8800b8747bceab93bb11594
sumitpd8 <sumitprasad423@gmail.com>
2026-04-24 22:27:53 +0530

    added AppConfig

--- backend/src/main/java/com/makaut_cse/sumit_kushal/finance_manager/FinanceManagerApplication.java
+++ backend/src/main/java/com/makaut_cse/sumit_kushal/finance_manager/FinanceManagerApplication.java
@@ -8,6 +8,6 @@ public class FinanceManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinanceManagerApplication.class, args);
-	}
 
+	}
 }
---
+++ backend/src/main/java/com/makaut_cse/sumit_kushal/finance_manager/config/AppConfig.java
@@ -0,0 +1,4 @@
+package com.makaut_cse.sumit_kushal.finance_manager.config;
+
+public class AppConfig {
+}
